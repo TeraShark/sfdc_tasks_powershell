c28daaaf551e6c54b6eee4be6eb9a6938d52ec64
Alleaume <c_alleaume@dell.com>
2022-09-21 09:38:27 -0500

    Refine options for end of task

--- sfdc_create_task.ps1
+++ sfdc_create_task.ps1
@@ -6,7 +6,7 @@
 # Variables for settings
 
 # Enter your SSO email address used in SFDC, or your username
-$username = 'your_corporate_email_address' 
+$username = '<your_corporate_email_address>' 
 # Customize this shortened task type list (ensuring that they match what's available in your SFDC instance)
 # This makes it easier to only display task types that are relevant to your daily tasks
 # Pay attention to the ordering and the colors used to display these tasks further below in this script
@@ -23,40 +23,45 @@ Write-Host "### ==========- SFDC Task Creator -========== ###" -ForegroundColor 
 Write-Host "### ========================================= ###" -ForegroundColor Cyan
 Write-Host " "
 
+$default_bgcolor = (get-host).UI.RawUI.BackgroundColor
 $repeat = $true
-while($repeat -eq $true){
-    # Prompt for Deal / Opportunity ID
-    $dealId = $(Write-Host "Enter Deal ID: " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host).Trim()
-    $isDealIdValid = $false
-    while($isDealIdValid -ne $true){
-        if ($dealId -match "^\d+$") {
-            $isDealIdValid = $true
-        } else {
-            Write-Host "ERROR: Deal ID [$dealId] is not valid. Please use a valid numeric Deal ID." -ForegroundColor Red
-            $dealId = $(Write-Host "Enter Deal ID: " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host).Trim()
+$same_opp = $false;
+while ($repeat -eq $true) {
+    if ($same_opp -eq $false) {
+        # Prompt for Deal / Opportunity ID
+        $dealId = $(Write-Host "Enter Deal ID: " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host).Trim()
+        $isDealIdValid = $false
+        while ($isDealIdValid -ne $true) {
+            if ($dealId -match "^\d+$") {
+                $isDealIdValid = $true
+            }
+            else {
+                Write-Host "ERROR: Deal ID [$dealId] is not valid. Please use a valid numeric Deal ID." -ForegroundColor Red
+                $dealId = $(Write-Host "Enter Deal ID: " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host).Trim()
+            }
         }
+
+        # Find opp ID by Deal ID
+        Write-Host "Fetching Opportunity..." -ForegroundColor Yellow
+        $opp = sfdx force:data:soql:query -u "$username" --query "SELECT ID, Name, AccountId FROM Opportunity WHERE Deal_ID__c='$dealId'" --json | ConvertFrom-Json
+
+        # Verify that the Opp was found
+        if ($opp.result.totalSize -lt 1) {
+            Write-Host "ERROR: Opportunity with Deal ID [$dealId] NOT found (or you don't have access)!" -ForegroundColor Red
+            Read-Host -Prompt "Press Enter to exit"
+            exit
+        }
+
+        # Set variables for future queries
+        $oppId = $opp.result.records.Id
+        $oppName = $opp.result.records.Name
+        $accountId = $opp.result.records.AccountId
+
+        # Find the associated account so that we can display for verification
+        $account = sfdx force:data:soql:query -u "$username" --query "SELECT Id, Name FROM Account WHERE Id='$accountId'" --json | ConvertFrom-Json
+        Write-Host " "
+        $accountName = $account.result.records.Name
     }
-
-    # Find opp ID by Deal ID
-    Write-Host "Fetching Opportunity..." -ForegroundColor Yellow
-    $opp = sfdx force:data:soql:query -u "$username" --query "SELECT ID, Name, AccountId FROM Opportunity WHERE Deal_ID__c='$dealId'" --json | ConvertFrom-Json
-
-    # Verify that the Opp was found
-    if ($opp.result.totalSize -lt 1){
-        Write-Host "ERROR: Opportunity with Deal ID [$dealId] NOT found (or you don't have access)!" -ForegroundColor Red
-        Read-Host -Prompt "Press Enter to exit"
-        exit
-    }
-
-    # Set variables for future queries
-    $oppId = $opp.result.records.Id
-    $oppName = $opp.result.records.Name
-    $accountId = $opp.result.records.AccountId
-
-    # Find the associated account so that we can display for verification
-    $account = sfdx force:data:soql:query -u "$username" --query "SELECT Id, Name FROM Account WHERE Id='$accountId'" --json | ConvertFrom-Json
-    Write-Host " "
-    $accountName = $account.result.records.Name
 
     # Retrieve full opp Record by ID (if we need specific details)
     # $opp = sfdx force:data:record:get -s Opportunity -i "$oppId" -u "$username" --json | ConvertFrom-Json
@@ -72,19 +77,19 @@ while($repeat -eq $true){
     # Establish whether or not to use today's date for the task
     $useToday = $(Write-Host "Use today's date for Task? (y/n): " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host) 
 
-    if ($useToday -eq 'n'){
+    if ($useToday -eq 'n') {
         $inDate = Read-Host -Prompt "Enter Task Date in the format [MM/dd/yy] (eg. 10/31/22)"
         $customDate = [datetime]::ParseExact($inDate, 'MM/dd/yy', $null)
         $now = $customDate.ToString("yyyy-MM-dd")
         Write-Host "Task Date: $now" -ForegroundColor Blue
         $nowTime = $customDate.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ss.fffffffZ" )
     }
-    Write-Host " "
+    Write-Host " " -BackgroundColor $default_bgcolor
 
-    Write-Host "Choose Task Type:" -ForegroundColor Yellow -BackgroundColor DarkGreen
-
+    Write-Host "Choose Task Type:" -NoNewLine -ForegroundColor Yellow -BackgroundColor DarkGreen
+    Write-Host " " -BackgroundColor $default_bgcolor
     # Display the task types in two colors to immediately differentiate from common vs less common task types
-    Write-Host "1. $($taskTypes[0])" -ForegroundColor White
+    Write-Host "1. $($taskTypes[0])" -ForegroundColor White 
     Write-Host "2. $($taskTypes[1])" -ForegroundColor White
     Write-Host "3. $($taskTypes[2])" -ForegroundColor Yellow
     Write-Host "4. $($taskTypes[3])" -ForegroundColor Yellow
@@ -92,15 +97,15 @@ while($repeat -eq $true){
     Write-Host "6. $($taskTypes[5])" -ForegroundColor Yellow
     Write-Host "7. $($taskTypes[6])" -ForegroundColor Yellow
 
-    Write-Host " "
+    Write-Host " " -BackgroundColor $default_bgcolor
     $taskId = $(Write-Host "Enter Task Type ID (eg 1): " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host) 
     $taskType = $taskTypes[$taskId - 1]
 
-    Write-Host "Selected Task Type: $taskType" -ForegroundColor Yellow
+    Write-Host "Selected Task Type: $taskType" -ForegroundColor Yellow -BackgroundColor $default_bgcolor
     Write-Host " "
     $descr = $(Write-Host "Enter Task Description: " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host)
 
-    Write-Host "Creating Task [$taskType]..." -ForegroundColor Yellow
+    Write-Host "Creating Task [$taskType]..." -ForegroundColor Yellow -BackgroundColor $default_bgcolor
     Write-Host " "
 
     # Create the task and display a summary
@@ -117,21 +122,33 @@ while($repeat -eq $true){
         Write-Host "$oppName" -ForegroundColor Yellow
         Write-Host "Deal ID: " -ForegroundColor Green -NoNewline
         Write-Host "$dealId" -ForegroundColor Yellow
-    } else {
+    }
+    else {
         Write-Host "There was an error creating your task!" -ForegroundColor Red
     }
-
-    # Prompt whether to open the newly created task in a browser
-    $openUrl = $(Write-Host "Open Task in Browser? (y/n): " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host)
-    if ($openUrl.ToLower() -eq 'y'){
-        Write-Host "### ===============  Done  =============== ###" -ForegroundColor Yellow
-        Start-Process "https://dell.lightning.force.com/lightning/r/Task/$($task.result.Id)/view?ws=%2Flightning%2Fr%2FOpportunity%2F$oppId%2Fview"
-        exit
-    }
-
-    $again = $(Write-Host "Create another Task? (y/n): " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host)
-    if ($again.ToLower() -ne 'y'){
-        $repeat = $false
+    Write-Host " " -BackgroundColor $default_bgcolor
+    Write-Host "==> Done. What's next?" -NoNewLine -ForegroundColor Yellow -BackgroundColor DarkGreen
+    Write-Host " " -BackgroundColor $default_bgcolor
+    Write-Host "1. Open the recently created SFDC task in a browser" -ForegroundColor White
+    Write-Host "2. Create another task for the same Opportunity" -ForegroundColor White
+    Write-Host "3. Create a new task for a " -NoNewline -ForegroundColor White 
+    Write-Host "different " -ForegroundColor Yellow -NoNewline
+    Write-Host "opportunity" -ForegroundColor White 
+    Write-Host "4. Quit" -ForegroundColor White
+    Write-Host " " -BackgroundColor $default_bgcolor
+    $next = $(Write-Host "Choose an action (eg 1): " -ForegroundColor Yellow -BackgroundColor DarkGreen -NoNewLine; Read-Host)
+    Write-Host " " -BackgroundColor $default_bgcolor
+    $repeat = $false
+    $same_opp = $false;
+    switch ("$next") {
+        1 {
+            Write-Host "### ============  Opening Task in SFDC  ============ ###" -ForegroundColor Yellow;
+            Start-Sleep -Seconds 1
+            Start-Process "https://dell.lightning.force.com/lightning/r/Task/$($task.result.Id)/view?ws=%2Flightning%2Fr%2FOpportunity%2F$oppId%2Fview";
+            exit;
+        }
+        2 { $repeat = $true; $same_opp = $true; Break }
+        3 { $repeat = $true; $same_opp = $false; Break }
     }
 }
 Write-Host "### ===============  Done  =============== ###" -ForegroundColor Yellow
